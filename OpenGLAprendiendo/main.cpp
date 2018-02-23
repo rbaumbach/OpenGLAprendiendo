0e8e74f5e0639faeaec1856ff3aad00fce1d981f
Ryan <github@ryan.codes>
2018-02-23 09:33:05 -0700

    Add some references to README.md, update main.cpp to create an glfw window with background color

--- OpenGLAprendiendo/main.cpp
+++ OpenGLAprendiendo/main.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800;
+const GLint HEIGHT = 600;
+
+int main()
+{
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // this is required for macOS
+    
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Aprendiendo", NULL, NULL);
+    
+    // this is required for macOS due to retina screens
+    
+    int screenWidth;
+    int screenHeight;
+    
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+    
+    glViewport(0, 0, screenWidth, screenHeight);
+    
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+    }
+    
+    glfwTerminate();
+    
     return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # OpenGLAprendiendo
 
 Learning how to draw shapes using OpenGL on macOS
+
+# References
+
+[Learn OpenGL](https://learnopengl.com)
+[Setting up glfw and GLEW on macOS](https://www.youtube.com/watch?v=Tz0dq2krCW8)
