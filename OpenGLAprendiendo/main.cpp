8741099ef52e8b44ae4420825a2e89ae989d6630
Ryan <github@ryan.codes>
2018-02-27 08:56:30 -0700

    Add processInput function to add functionality to close window when ESC key is pressed

--- OpenGLAprendiendo/main.cpp
+++ OpenGLAprendiendo/main.cpp
@@ -26,6 +26,8 @@ const GLchar *fragmentShaderSource = "#version 330 core\n"
 "FragColor = vec4(ourColor, 1.0);\n"
 "}";
 
+void processInput(GLFWwindow *window);
+
 int main()
 {
     glfwInit();
@@ -155,7 +157,7 @@ int main()
 
     while(!glfwWindowShouldClose(window))
     {
-        glfwPollEvents();
+        processInput(window);
         
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -168,6 +170,7 @@ int main()
         glDrawArrays(GL_TRIANGLES, 0, 3);
         
         glfwSwapBuffers(window);
+        glfwPollEvents();
     }
     
     glDeleteVertexArrays(1, &VAO);
@@ -176,4 +179,10 @@ int main()
     glfwTerminate();
     
     return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
