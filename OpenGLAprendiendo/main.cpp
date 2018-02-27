5beace1b1be225c29b2cbf7681048c94a0ad0c2f
Ryan <github@ryan.codes>
2018-02-27 08:52:13 -0700

    Make triangle colorful using color data in vertices array

--- OpenGLAprendiendo/main.cpp
+++ OpenGLAprendiendo/main.cpp
@@ -10,16 +10,20 @@ const GLint HEIGHT = 600;
 
 const GLchar *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 position;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+"gl_Position = vec4(position, 1.0);\n"
+"ourColor = aColor;\n"
 "}";
 
 const GLchar *fragmentShaderSource = "#version 330 core\n"
+"in vec3 ourColor;\n"
 "out vec4 FragColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"FragColor = vec4(ourColor, 1.0);\n"
 "}";
 
 int main()
@@ -112,9 +116,10 @@ int main()
     glDeleteShader(fragmentShader);
     
     GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f, // bottom left
-        0.5f, -0.5f, 0.0f,  // bottom right
-        0.0f,  0.5f, 0.0f   // top
+        // positions          // colors
+        0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   // bottom left
+        0.0f,  0.5f, 0.0f,    0.0f, 0.0f, 1.0f    // top
     };
     
     GLuint VBO, VAO;
@@ -129,8 +134,15 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *)0);
+    // position attribute
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    
+    // color attribute
+    
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
     
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     
