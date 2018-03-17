fb6c5b03edfe517747b75f371067546ee9a5b689
Ryan <github@ryan.codes>
2018-03-17 10:01:27 -0600

    Add #ifdef for includes in ShaderManager class to for iOS cross platform compatability

--- OpenGLAprendiendo/Triangle/Shaders/ShaderManager.hpp
+++ OpenGLAprendiendo/Triangle/Shaders/ShaderManager.hpp
@@ -1,7 +1,17 @@
 #ifndef Shader_hpp
 #define Shader_hpp
 
-#include <GL/glew.h>
+// Note: See "TargetConditionals.h" for TARGET information
+
+#ifdef __APPLE__
+    #include "TargetConditionals.h"
+
+    #if TARGET_OS_OSX
+        #include <GL/glew.h>
+    #else
+        #include <OpenGLES/ES3/glext.h>
+    #endif
+#endif
 
 class ShaderManager
 {
